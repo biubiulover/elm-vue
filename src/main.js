ae74998945cfcf1315127ce5be5090c94d1ef512
biubiulover <gnkevin@126.com>
2019-03-03 11:49:21 +0800

    elm-vue first commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import FastClick from 'fastclick'
+import VueRouter from 'vue-router'
+import routes from './router/router'
+import './config/rem'
+
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function () {
+    FastClick.attach(document.body)
+  }, false)
+}
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes,
+  mode: 'hash',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      if (from.meta.keepAlive) {
+        from.meta.savedPosition = document.body.scrollTop
+      }
+      return {x: 0, y: to.meta.savedPosition || 0}
+    }
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/page/home/home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <head-top signin-up='home'>
+      <span slot="logo" class="head_logo" @click="reload">ele.me</span>
+    </head-top>
+    home
+  </div>
+</template>
+<script>
+import headTop from '../../components/header/head'
+export default {
+  name: 'home',
+  components: {
+    headTop
+  },
+  methods: {
+    reload () {
+      window.location.reload()
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @import '../../style/mixin';
+  .head_logo {
+    left: 0.4rem;
+    font-weight: 400;
+    @include sc(0.7rem, #fff);
+    @include wh(2.3rem, 0.7rem);
+    @include ct;
+  }
+</style>
